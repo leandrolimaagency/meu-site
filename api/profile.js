26a7b72ea9ef61b4a4879db0caf849f2cd2c946a
Ana Barbara De Souza Duarte <anaduarte@MacBook-Air-de-Ana.local>
2026-08-11 16:42:29 -0300

    Corrige função serverless do perfil

--- api/profile.js
+++ api/profile.js
@@ -15,7 +15,7 @@
 
 const IG_APP_ID = "936619743392459"; // app id público do web client do IG
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
